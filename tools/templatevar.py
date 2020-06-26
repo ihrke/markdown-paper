1290c168e5f685b7cffdd8d4b250a8a085facfa0
Matthias Mittner <matthias.mittner@uit.no>
2020-06-26 13:20:46 +0200

    script to generate used variables for each template

---
+++ tools/templatevar.py
@@ -0,0 +1,23 @@
+"""
+Find and print all variables provided for each template.
+"""
+import re
+import numpy as np
+
+exec(open("conf.py").read())
+
+for template,d in templates.items():
+    print( ("{template}\n"+"-"*len(template) ).format(template=template))
+    text=open(d["template_file"]).read()
+    
+    vars=re.findall("\$.*?\$", text)
+    # drop the $ delimiters
+    vars=[var[1:-1] for var in vars]
+    # drop all vars that begin with one of these strings
+    dropstrings=["if(", "else", "sep", "for(", "end"]
+    vars=[var for var in vars if np.all([not var.startswith(s) for s in dropstrings]) and len(var)>0]
+    # get rid of duplicates and sort
+    vars=sorted(set(vars))
+    for var in vars:
+        print("- %s"%var)
+    print("")
