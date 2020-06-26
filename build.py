aa25bac14600e276fc25278f3477c7d9c3a0d020
Matthias Mittner <matthias.mittner@uit.no>
2020-06-26 12:58:07 +0200

    moved configuration into conf.py

--- build.py
+++ build.py
@@ -5,25 +5,7 @@ import json
 from subprocess import Popen, PIPE
 from pandocfilters import stringify
 
-default_template="elsevier"
-templates={
-  "elsevier":{
-    "template_file":"templates/elsarticle-template-1-num.latex",
-    "bibtexcommand":"bibtex",
-    "natbib":"--natbib"
-    },
-  "scientific_reports":{
-    "template_file":"templates/scientific_reports.latex",
-    "bibtexcommand":"biber",
-    "natbib":"--biblatex"
-    },
-  "springer":{
-    "template_file":"templates/springer_svjour3.latex",
-    "bibtexcommand":"bibtex",
-    "natbib":"--natbib"
-    }
-  }
-
+exec(open("conf.py").read())
 mdfile=sys.argv[1]
 
 ## convert md to JSON
---
+++ conf.py
@@ -0,0 +1,18 @@
+default_template="elsevier"
+templates={
+  "elsevier":{
+    "template_file":"templates/elsarticle-template-1-num.latex",
+    "bibtexcommand":"bibtex",
+    "natbib":"--natbib"
+    },
+  "scientific_reports":{
+    "template_file":"templates/scientific_reports.latex",
+    "bibtexcommand":"biber",
+    "natbib":"--biblatex"
+    },
+  "springer":{
+    "template_file":"templates/springer_svjour3.latex",
+    "bibtexcommand":"bibtex",
+    "natbib":"--natbib"
+    }
+  }
