bd4a7b8e672fc029e5edb9a7586c529397965de9
Matthias Mittner <matthias.mittner@uit.no>
2020-06-26 15:50:50 +0200

    Update thumbnails.sh

--- tools/thumbnails.sh
+++ tools/thumbnails.sh
@@ -5,5 +5,5 @@ for f in `ls pdf/`
 do
     BASE="$(basename $f .pdf)"
     convert pdf/$f\[0\] -resize 75% pics/$BASE.png
-    echo "![$BASE](pics/$BASE.pdf)"
+    echo "![$BASE](pics/$BASE.png)"
 done;
